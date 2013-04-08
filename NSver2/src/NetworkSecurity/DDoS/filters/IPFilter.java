113dcc36930e3dceeec3106f765463c9db3fe344
ShuoLi <dantein88@gmail.com>
2013-04-08 04:27:00 -0400

    IP filter done

---
+++ NSver2/src/NetworkSecurity/DDoS/filters/IPFilter.java
@@ -0,0 +1,52 @@
+package NetworkSecurity.DDoS.filters;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import NetworkSecurity.DDoS.logic.IPController;
+
+
+public class IPFilter implements Filter{
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		
+        HttpServletRequest request = (HttpServletRequest) req;
+        
+        //Get the IP address of client machine.
+        String ipAddress = request.getRemoteAddr();
+        HttpServletResponse httpResp = null;
+
+        if (res instanceof HttpServletResponse)
+          httpResp = (HttpServletResponse) res;
+        
+        if(!IPController.checkAccess(ipAddress)) {
+        	httpResp.sendError(HttpServletResponse.SC_FORBIDDEN,
+        	          "That means goodbye forever!");
+         }
+        //Log the IP address and current timestamp.
+        System.err.println("IP "+ipAddress + ", Time "
+                            + new Date().toString());
+         IPController.increase(ipAddress);
+        chain.doFilter(req, res);
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+		//Get init parameter
+		
+	}
+
+}
---
+++ NSver2/src/NetworkSecurity/DDoS/logic/IPController.java
@@ -0,0 +1,28 @@
+package NetworkSecurity.DDoS.logic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IPController {
+	private static Map<String,Integer> connections=new HashMap<String,Integer>();
+	private static List<String> blockIPS = new ArrayList<String>();
+	
+	public static void increase(String IP){
+		if(connections.containsKey(IP)){
+			if(connections.get(IP)>3){
+				blockIPS.add(IP);
+				System.err.println("This is an DDoS attack");
+			}
+			connections.put(IP, connections.get(IP)+1);
+		}else
+			connections.put(IP, 1);
+	}
+	
+	public static boolean checkAccess(String IP){
+		if(blockIPS.contains(IP))
+			return false;
+		return true;
+	}
+}
